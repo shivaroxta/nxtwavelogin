1d8e6f70b90cf0b5de1744f638f2df674316b409
shiva <pallewarshiva@gmail.com>
2022-12-26 03:56:13 +0000

    destinationsearch

---
+++ src/components/Cart/index.js
@@ -0,0 +1,16 @@
+import Header from '../Header'
+import './index.css'
+
+const Cart = () => (
+  <div className="cart-container">
+    <Header />
+
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+      className="cart-image"
+      alt="cart"
+    />
+  </div>
+)
+
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,16 @@
+import Header from '../Header'
+import './index.css'
+
+const Products = () => (
+  <div className="products-container">
+    <Header />
+
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+      className="products-image"
+      alt="products"
+    />
+  </div>
+)
+
+export default Products
